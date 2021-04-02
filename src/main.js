70a0fe16baeb7f66c890f38eeaf5e7e03237e08a
Sydney Bear <sbear3915@gmail.com>
2021-04-01 19:11:23 -0600

    Create event listener for make cover button

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ var bookTitle = document.querySelector(".cover-title");
 var bookTagline1 = document.querySelector(".tagline-1");
 var bookTagline2 = document.querySelector(".tagline-2");
 var randomCoverButton = document.querySelector(".random-cover-button");
+//create a variable and grab the make your own cover button using querySelector
+//create a variable and grab the form Section
+//create a varibale andgrab the view home-view Section
+var makeCoverButton = document.querySelector(".make-new-button");
+var form = document.querySelector(".form-view");
+var homeView = document.querySelector(".home-view");
+var homeButton = document.querySelector(".home-button");
+var saveCoverButton = document.querySelector(".save-cover-button");
+
 
 // We've provided a few variables below
 var savedCovers = [
@@ -17,6 +26,7 @@ var currentCover;
 changeCover();
 
 randomCoverButton.addEventListener('click', changeCover);
+makeCoverButton.addEventListener('click', toggleMakeCoverVisible);
 
 // Create your event handlers and other functions here 👇
 
@@ -36,6 +46,13 @@ function changeCover() {
   bookTagline2.innerText = newDescriptor2;
   currentCover = new Cover(newCover, newTitle, newDescriptor1, newDescriptor2);
   return currentCover;
+}
+//create a function that will toggle the home button, the main cover, the FORM
+//show new random cover and save cover
+
+function toggleMakeCoverVisible(){
+ homeButton.classList.remove("hidden");
+
 }
 
 // We've provided one function to get you started
