27ad3de4ef0e777b1d1e051274aae203ec3fa5b8
Dean Cook <deancook371@gmail.com>
2021-04-04 09:51:06 -0600

    Clean up code in functions

--- src/main.js
+++ src/main.js
@@ -20,6 +20,7 @@ var createNewBookButton = document.querySelector(".create-new-book-button");
 var savedCoversSection = document.querySelector(".saved-covers-section");
 
 // We've provided a few variables below
+
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
@@ -38,21 +39,20 @@ createNewBookButton.addEventListener("click", function() {
   event.preventDefault();
 });
 saveCoverButton.addEventListener("click", addCurrentCover);
-savedCoversSection.addEventListener("dblclick", function(e){
+savedCoversSection.addEventListener("dblclick", function(e) {
   deleteSavedCover(e)
 });
+
 // Create your event handlers and other functions here 👇
 
 function changeCover() {
-  var coverIndex = getRandomIndex(covers);
-  var newCover = covers[coverIndex];
-  var titleIndex = getRandomIndex(titles);
-  var newTitle = titles[titleIndex];
-  var descriptorIndex1 = getRandomIndex(descriptors);
-  var newDescriptor1 = descriptors[descriptorIndex1];
-  var descriptorIndex2 = getRandomIndex(descriptors);
-  var newDescriptor2 = descriptors[descriptorIndex2];
-  //note: for refactoring, include if/else that prevents descriptors from being the same word
+  var newCover = covers[getRandomIndex(covers)];
+  var newTitle = titles[getRandomIndex(titles)];
+  var newDescriptor1 = descriptors[getRandomIndex(descriptors)];
+  var newDescriptor2 = descriptors[getRandomIndex(descriptors)];
+  if (newDescriptor1 === newDescriptor2) {
+    newDescriptor2 = descriptors[getRandomIndex(descriptors)];
+  }
   bookCover.src = newCover;
   bookTitle.innerText = newTitle;
   bookTagline1.innerText = newDescriptor1;
@@ -61,46 +61,30 @@ function changeCover() {
   return currentCover;
 }
 
-//note- for refactoring, toggle visibility with single function that takes parameter?
-
 function makeCoverView(){
  homeButton.classList.remove("hidden");
+ form.classList.remove("hidden");
  saveCoverButton.classList.add("hidden");
  randomCoverButton.classList.add("hidden");
- form.classList.remove("hidden");
  homeView.classList.add("hidden");
  saveCoverPage.classList.add("hidden");
 }
 
 function savedCoverView() {
   homeButton.classList.remove("hidden");
+  saveCoverPage.classList.remove("hidden");
   randomCoverButton.classList.add("hidden");
   saveCoverButton.classList.add("hidden");
-  saveCoverPage.classList.remove("hidden");
   homeView.classList.add("hidden");
   form.classList.add("hidden");
   createMiniCover();
 }
 
-function createMiniCover(){
-  savedCoversSection.innerHTML=``;
-  for (var i = 0; i < savedCovers.length; i++) {
-    console.log(i);
-    var miniCovers =`<section class="mini-cover" id="${i}">
-      <img id="${i}" class="cover-image" src=${savedCovers[i].cover}>
-      <h2 class="cover-title">${savedCovers[i].title}</h2>
-      <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
-    </section>`
-    savedCoversSection.insertAdjacentHTML("afterbegin", miniCovers);
-  }
-    //show the cover object at [i] in the saved covers view
-}
-
 function homePageView() {
-  homeButton.classList.add("hidden");
   saveCoverButton.classList.remove("hidden");
   randomCoverButton.classList.remove("hidden");
   homeView.classList.remove("hidden");
+  homeButton.classList.add("hidden");
   saveCoverPage.classList.add("hidden");
   form.classList.add("hidden");
 }
@@ -112,8 +96,7 @@ function createNewBook() {
   var inputDesc2 = userDescriptor2.value;
   covers.push(inputCover);
   titles.push(inputTitle);
-  descriptors.push(inputDesc1);
-  descriptors.push(inputDesc2);
+  descriptors.push(inputDesc1, inputDesc2);
   currentCover = new Cover(inputCover, inputTitle, inputDesc1, inputDesc2);
   bookCover.src = inputCover;
   bookTitle.innerText = inputTitle;
@@ -131,13 +114,25 @@ function addCurrentCover() {
   }
 }
 
-function deleteSavedCover(e){
-console.log("e", e.target.id);
-savedCovers.splice(e.target.id,1);
-console.log(savedCovers);
-createMiniCover();
+function createMiniCover() {
+  savedCoversSection.innerHTML = ``;
+  for (var i = 0; i < savedCovers.length; i++) {
+    var miniCovers =`<section class="mini-cover" id="${i}">
+      <img id="${i}" class="cover-image" src=${savedCovers[i].cover}>
+      <h2 class="cover-title">${savedCovers[i].title}</h2>
+      <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
+    </section>`
+    savedCoversSection.insertAdjacentHTML("afterbegin", miniCovers);
+  }
 }
+
+function deleteSavedCover(e) {
+  savedCovers.splice(e.target.id, 1);
+  createMiniCover();
+}
+
 // We've provided one function to get you started
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
