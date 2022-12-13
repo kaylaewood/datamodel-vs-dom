cf8f642a9dcd76ea4f115cdcf8d5c13fd0fd18c3
Kayla Wood <kaylaewood@gmail.com>
2022-12-13 16:47:59 -0700

    Change activity so focus is on saving and deleting

--- src/main.js
+++ src/main.js
@@ -2,56 +2,51 @@ var bookCover = document.querySelector(".cover-image");
 var bookTitle = document.querySelector(".cover-title");
 var bookTagline1 = document.querySelector(".tagline-1");
 var bookTagline2 = document.querySelector(".tagline-2");
-var randomCoverButton = document.querySelector(".random-cover-button");
-var makeCoverButton = document.querySelector(".make-new-button");
-var form = document.querySelector(".form-view");
-var homeView = document.querySelector(".home-view");
-var homeButton = document.querySelector(".home-button");
-var viewSavedButton = document.querySelector(".view-saved-button");
+var homePage = document.querySelector(".home-view");
+var viewHomePageButton = document.querySelector(".home-button");
+var makeRandomCoverButton = document.querySelector(".random-cover-button");
 var saveCoverButton = document.querySelector(".save-cover-button");
-var saveCoverPage = document.querySelector(".saved-view");
-var userCover = document.querySelector(".user-cover");
-var userTitle = document.querySelector(".user-title");
-var userDescriptor1 = document.querySelector(".user-desc1");
-var userDescriptor2 = document.querySelector(".user-desc2");
-var createNewBookButton = document.querySelector(".create-new-book-button");
-var savedCoversSection = document.querySelector(".saved-covers-section");
+var savedCoversPage = document.querySelector(".saved-view");
+var savedCoversContainer = document.querySelector(".saved-covers-section");
+var viewSavedCoversButton = document.querySelector(".view-saved-button");
 
-var savedCovers = [];
-var currentCover;
 
-// PART 1: DISPLAYING ONE RANDOM COVER
+// PART 1: RENDERING COVERS ON MAIN PAGE
 
-window.addEventListener("load", changeCover);
+// Take a minute to look at the code below. Pay attention to:
+  // - What is representing the Data Model?
+  // - When/where are we updating the Data Model?
+  // - When/where are we updating the DOM?
+  // - Is the DOM update dependent on the Data Model?
 
-randomCoverButton.addEventListener("click", changeCover);
+var currentCover; // Data Model 
 
-function changeCover() {
-  var newCover = covers[getRandomIndex(covers)];
-  var newTitle = titles[getRandomIndex(titles)];
-  var newDescriptor1 = descriptors[getRandomIndex(descriptors)];
-  var newDescriptor2 = descriptors[getRandomIndex(descriptors)];
+window.addEventListener("load", function() {
+  makeRandomCover(); // Upating the Data Model
+  renderCurrentCover(); // Updating the DOM
+});
 
-  if (newDescriptor1 === newDescriptor2) {
-    newDescriptor2 = descriptors[getRandomIndex(descriptors)];
-  }
+makeRandomCoverButton.addEventListener("click", function() {
+  makeRandomCover(); // Upating the Data Model
+  renderCurrentCover(); // Updating the DOM
+});
 
-  bookCover.src = newCover;
-  bookTitle.innerText = newTitle;
-  bookTagline1.innerText = newDescriptor1;
-  bookTagline2.innerText = newDescriptor2;
-  
-  currentCover = new Cover(newCover, newTitle, newDescriptor1, newDescriptor2);
+function makeRandomCover() { // Updating the Data Model (currentCover)
+  currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
+}
+
+function renderCurrentCover() { // Using the Data Model (currentCover) to update the DOM
+  bookCover.src = currentCover.cover;
+  bookTitle.innerText = currentCover.title;
+  bookTagline1.innerText = currentCover.tagline1;
+  bookTagline2.innerText = currentCover.tagline2;
 }
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-// 1. What represents the data model in this section of the code?
-// 2. Currently, are we updating the data model when a random poster is created?
-// 3. Currently, are we updating the DOM using the data model?
-// 4. What changes should we make?
+// PART 1 STOPS HERE!
 
 
 
@@ -60,94 +55,61 @@ function getRandomIndex(array) {
 
 
 
+// PART 2: SAVING COVERS
 
+// Add code below so that:
+  // When the user clicks "Save Cover," the cover is saved in the Data Model AND the saved poster is added to the DOM (in the saved covers view)
 
-// PART 2: CREATING NEW COVER
+// Notes:
+  // The page view change is already functional (bottom of this file) - You do not need to do anything for that
+  // Don't worry about duplicates - It's okay if you can save duplicate covers
 
-createNewBookButton.addEventListener("click", createNewBook);
+var savedCovers = []; // Data Model
 
-function createNewBook() {
-  event.preventDefault();
-
-  var inputCover = userCover.value;
-  var inputTitle = userTitle.value;
-  var inputDesc1 = userDescriptor1.value;
-  var inputDesc2 = userDescriptor2.value;
-
-  covers.push(inputCover);
-  titles.push(inputTitle);
-  descriptors.push(inputDesc1, inputDesc2);
-
-  currentCover = new Cover(inputCover, inputTitle, inputDesc1, inputDesc2);
-
-  bookCover.src = inputCover;
-  bookTitle.innerText = inputTitle;
-  bookTagline1.innerText = inputDesc1;
-  bookTagline2.innerText = inputDesc2;
-
-  displayHomePage();
-}
-
-// 1. What represents the data model in this section of the code?
-// 2. Currently, are we updating the data model when a poster is created?
-// 3. Currently, are we updating the DOM using the data model?
-// 4. What changes should we make? (HINT: Is there an existing function we can reuse here?)
-
-
-
-
-
-
-
-
-
-
-// PART 3: SAVING COVERS
-
-saveCoverButton.addEventListener("click", saveCurrentCover);
-
-function saveCurrentCover() {
-  if (!savedCovers.includes(currentCover)) {
-    savedCovers.push(currentCover);
-
-    savedCoversSection.innerHTML +=
-    `
-    <section class="mini-cover" id="${savedCovers.length}">
-      <img class="cover-image" src=${currentCover.cover}>
-      <h2 class="cover-title">${currentCover.title}</h2>
-      <h3 class="tagline">A tale of <span class="tagline-1">${currentCover.tagline1}</span> and <span class="tagline-2">${currentCover.tagline2}</span></h3>
-    </section>
-    `
-  }
-}
-
-// 1. What represents the data model in this section of the code?
-// 2. Currently, are we updating the data model when a poster is saved?
-// 3. Currently, are we updating the DOM using the data model?
-// 4. What changes should we make?
-
-
-
-
-
-
-
-
-
-// PART 4: DELETING COVERS
-
-savedCoversSection.addEventListener("dblclick", function(e) {
-  deleteSavedCover(e);
+saveCoverButton.addEventListener("click", function() {
+  saveCurrentCover(); // Update the Data Model
+  renderSavedCovers(); // Update the DOM
 });
 
-function deleteSavedCover(e) {
-  e.target.closest('section').remove();
+function saveCurrentCover() {
+  // add code here
 }
 
-// 1. What represents the data model in this section of the code?
-// 2. Currently, are we updating the data model when a poster is deleted?
-// 3. Currently, are we updating the DOM using the data model?
-// 4. What changes should we make? (HINT: Is there an existing function we can reuse here?)
+function renderSavedCovers() {
+  // add code here (some starter code provided below)
+  
+  // savedCoversContainer.innerHTML += 
+  //   `<section class="mini-cover" id="${Date.now()}">
+      // <img class="cover-image" src=${/* cover */}>
+      // <h2 class="cover-title">${/* title */}</h2>
+      // <h3 class="tagline">A tale of <span class="tagline-1">${/* tagline1 */}</span> and <span class="tagline-2">${/* tagline2 */}</span></h3>
+  //   </section>`
+}
+
+// PART 2 STOPS HERE!
+
+
+
+
+
+
+
+
+// PART 3: DELETING COVERS
+
+// Add code below so that:
+  // When the user double clicks the saved poster, the poster is deleted from the Data Model AND the deleted poster no longer appears on the page
+
+savedCoversContainer.addEventListener("dblclick", function(event) {
+  deleteSavedCover(event.target.id);
+  // what else should we invoke here to update the DOM?
+});
+
+function deleteSavedCover(id) {
+  // add code here to update the Data Model
+}
+
+// PART 3 STOPS HERE!
 
 
 
@@ -158,37 +120,25 @@ function deleteSavedCover(e) {
 
 
 
-// CHANGING VIEWS (not concerned about this in this lesson!)
+// CHANGING VIEWS (We are not concerned about this in this lesson!)
 
-viewSavedButton.addEventListener("click", displaySavedCovers);
-
-makeCoverButton.addEventListener("click", displayForm);
-
-homeButton.addEventListener("click", displayHomePage);
+viewSavedCoversButton.addEventListener("click", displaySavedCovers);
+viewHomePageButton.addEventListener("click", displayHomePage);
 
 function displaySavedCovers() {
-  homeButton.classList.remove("hidden");
-  saveCoverPage.classList.remove("hidden");
-  randomCoverButton.classList.add("hidden");
+  viewHomePageButton.classList.remove("hidden");
+  savedCoversPage.classList.remove("hidden");
+  makeRandomCoverButton.classList.add("hidden");
   saveCoverButton.classList.add("hidden");
-  homeView.classList.add("hidden");
-  form.classList.add("hidden");
-}
-
-function displayForm(){
- homeButton.classList.remove("hidden");
- form.classList.remove("hidden");
- saveCoverButton.classList.add("hidden");
- randomCoverButton.classList.add("hidden");
- homeView.classList.add("hidden");
- saveCoverPage.classList.add("hidden");
+  homePage.classList.add("hidden");
+  viewSavedCoversButton.classList.add("hidden");
 }
 
 function displayHomePage() {
   saveCoverButton.classList.remove("hidden");
-  randomCoverButton.classList.remove("hidden");
-  homeView.classList.remove("hidden");
-  homeButton.classList.add("hidden");
-  saveCoverPage.classList.add("hidden");
-  form.classList.add("hidden");
+  makeRandomCoverButton.classList.remove("hidden");
+  homePage.classList.remove("hidden");
+  viewHomePageButton.classList.add("hidden");
+  savedCoversPage.classList.add("hidden");
+  viewSavedCoversButton.classList.remove("hidden");
 }
